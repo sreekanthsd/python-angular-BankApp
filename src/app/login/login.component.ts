304e8652be7e762c5fa5d46a368cf3085af60bb3
Sreekanth.v <sreeskavettikatt@gmail.com>
2021-05-26 02:17:56 +0700

    one way Data binding- interpolation,property and event binding and update the view of login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  aim="Your perfect banking partner";
+  accountNumber="Account Number Please";
+  accnum="";
+  pswd="";
+
+  accountdetails : any= {
+    1000: { name: "ajay", acno: 1000, password: "testone", balance: 5000 },
+    1001: { name: "vijay", acno: 1001, password: "testtwo", balance: 4000 },
+    1002: { name: "ram", acno: 1002, password: "testthree", balance: 6000 },
+    1003: { name: "ravi", acno: 1003, password: "testfour", balance: 7000 },
+}
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  accnoChange(event:any){
+    this.accnum=event.target.value;
+    console.log(this.accnum);
+  }
+
+  pwdChange(event:any){
+    this.pswd=event.target.value;
+    console.log(this.pswd);
+  }
+  
+  login(){
+      alert("Login clicked")
+      let acno = this.accnum
+      let pwd = this.pswd
+      let details = this.accountdetails 
+      if (acno in details) {
+          if (pwd == details[acno]["password"]) {
+              alert("login success");
+          }
+          else {
+              alert("incorrect password");
+          }
+      }
+      else {
+          alert("invalid accountnumber");
+
+      }
+
+  }
 }
